19ffa0376fe7d9080ece231cfcd4e1ccec15bc76
werochauol <123185536+werochauol@users.noreply.github.com>
2024-03-22 20:11:46 -0300

    Update dag.py

--- dag.py
+++ dag.py
@@ -2,3 +2,14 @@ from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1),
+}
